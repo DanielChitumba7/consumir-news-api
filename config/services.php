ffcba7860aa9bb695bf7b24dd7555afa567970b0
DanielChitumba7 <DanielChitumba@outlook.com>
2025-06-09 20:41:36 +0100

    feat: Add Api Key

--- config/services.php
+++ config/services.php
@@ -34,5 +34,9 @@ return [
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
+    //Api Key news-api
+    'newsapi' => [
+    'key' => env('NEWS_API_KEY'),
+],
 
 ];
